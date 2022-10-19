1bfd7294afdcf990622bfafcbdc863c3439cc2b9
Андрей хухарев <alf.master.mail@gmail.com>
2022-10-19 20:41:37 +0300

    Initial commit

---
+++ src/main/java/homeWork1/task1.java
@@ -0,0 +1,18 @@
+package homeWork1;
+
+import java.util.Arrays;
+
+public class task1 {
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        int[] arr = buildArray(nums);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < (nums.length); i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
